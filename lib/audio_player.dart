3f60ec97b52926d715174bf2e3318e8da8274fc0
Josephin Razafindrakoto <razafindrakotojosephin@gmail.com>
2024-08-03 04:05:23 +0300

    added audio_service

--- lib/audio_player.dart
+++ lib/audio_player.dart
@@ -3,6 +3,7 @@ library audio_player;
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
